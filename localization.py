0301696f2b8842e617ff7241372159c801bffe0c
Felix Taubner <ftaubner@ethz.ch>
2020-05-08 14:39:10 +0200

    Matching with labels.

--- localization.py
+++ localization.py
@@ -21,9 +21,14 @@ def get_colors():
 
 
 class LandmarkRenderer:
-    def __init__(self, landmarks, labels, frame_lms, frame_labels, label_colors):
+    def __init__(self, landmarks, labels, all_landmarks, all_labels, frame_lms, frame_labels, matched_lms, label_colors):
         self.landmarks = landmarks
         self.lm_labels = labels
+
+        self.all_landmarks = all_landmarks
+        self.all_lm_labels = all_labels
+
+        self.matched_lms = matched_lms
 
         self.frame_lms = frame_lms
         self.frame_labels = frame_labels
@@ -33,8 +38,11 @@ class LandmarkRenderer:
         self.render_single_frame = False
         self.render_pose_connect = False
         self.render_boxes = False
+        self.render_all = False
+        self.render_matched = False
 
         self.landmark_geometries = render_landmarks(self.landmarks, self.lm_labels, self.label_colors)
+        self.all_landmark_geometries = render_landmarks(self.all_landmarks, self.all_lm_labels, self.label_colors)
         self.frame_geometries = [render_landmarks(frame_lms[i], frame_labels[i], self.label_colors, size=0.1) for i in
                                  range(self.frame_count)]
 
@@ -48,11 +56,11 @@ class LandmarkRenderer:
 
         vis.register_key_callback(ord("F"), self.get_switch_index_callback(forward=True))
         vis.register_key_callback(ord("D"), self.get_switch_index_callback(forward=False))
-        # vis.register_key_callback(ord("A"), self.get_toggle_show_all_callback())
-        # vis.register_key_callback(ord("N"), self.get_toggle_connect_callback())
+        vis.register_key_callback(ord("A"), self.get_toggle_show_all_callback())
+        vis.register_key_callback(ord("M"), self.get_toggle_matched_callback())
         vis.register_key_callback(ord("S"), self.get_screen_cap_callback())
 
-        print("Press 'A' to toggle between show all and show only one frame.")
+        print("Press 'A' to toggle between show merged or show all landmarks.")
         print("Press 'F' to switch to next frame.")
         print("Press 'D' to switch to previous frame.")
         print("Press 'S' to capture screenshot.")
@@ -62,7 +70,6 @@ class LandmarkRenderer:
 
         for geometry in self.frame_geometries[self.pointer]:
             vis.add_geometry(geometry)
-            print("Render some boy")
 
         vis.add_geometry(self.ground_grid)
 
@@ -73,8 +80,17 @@ class LandmarkRenderer:
         view = vis.get_view_control().convert_to_pinhole_camera_parameters()
         vis.clear_geometries()
 
-        for geometry in self.landmark_geometries:
-            vis.add_geometry(geometry)
+        if self.render_all:
+            for geometry in self.all_landmark_geometries:
+                vis.add_geometry(geometry)
+        else:
+            if self.render_matched:
+                for i, geometry in enumerate(self.landmark_geometries):
+                    if i in self.matched_lms:
+                        vis.add_geometry(geometry)
+            else:
+                for geometry in self.landmark_geometries:
+                    vis.add_geometry(geometry)
 
         for geometry in self.frame_geometries[self.pointer]:
             vis.add_geometry(geometry)
@@ -86,16 +102,16 @@ class LandmarkRenderer:
         vis.update_renderer()
         vis.get_view_control().convert_from_pinhole_camera_parameters(view)
 
-    def get_toggle_connect_callback(self):
-        def toggle_connect(vis):
-            self.render_pose_connect = not self.render_pose_connect
+    def get_toggle_matched_callback(self):
+        def toggle_matched(vis):
+            self.render_matched = not self.render_matched
             self.update_render(vis)
 
-        return toggle_connect
+        return toggle_matched
 
     def get_toggle_show_all_callback(self):
         def toggle_show_all(vis):
-            self.render_single_frame = not self.render_single_frame
+            self.render_all = not self.render_all
             self.update_render(vis)
 
         return toggle_show_all
@@ -234,36 +250,68 @@ class Map:
         self.triangles = {}
         # The maximum viewing distance for valid landmarks is lets say 50 m.
         self.max_dist = 50.
-        self.octtree = OctTree(min_bound=np.zeros((3,)), max_bound=np.ones((3,)) * self.max_dist, depth=7)
+        self.oct_trees = {}
+        self.all_landmarks = []
+        self.all_landmark_labels = []
 
     def load_landmarks(self, frame_lms, frame_labels):
-        self.landmarks = np.vstack(frame_lms)
-        self.landmark_labels = np.concatenate(frame_labels, axis=0)
+        map_landmarks = np.vstack(frame_lms)
+        map_landmark_labels = np.concatenate(frame_labels, axis=0)
 
-        print("Number of landmarks: {}".format(self.landmarks.shape[0]))
+        # Safe old landmarks for later.
+        self.all_landmarks = np.copy(map_landmarks)
+        self.all_landmark_labels = np.copy(map_landmark_labels)
+
+        # Set label 1 (green) and label 15 (yellow) as same label (they often appear together)
+        map_landmark_labels[np.where(map_landmark_labels == 1)] = 15
+
+        n_landmarks_before = map_landmarks.shape[0]
+        print("Number of map landmarks: {}".format(map_landmarks.shape[0]))
         # Use midpoints of merged bounding boxes as landmark.
         # self.landmarks = (bboxes[:, :3] + bboxes[:, 3:6]) / 2.
         # self.landmark_labels = labels
 
-        # Check how many landmarks below threshold.
-        check_dist = 0.0
-        num_below = 0
-        lms_to_delete = []
-        for i in range(self.landmarks.shape[0] - 1):
-            for j in range(i + 1, self.landmarks.shape[0]):
-                if np.linalg.norm(self.landmarks[i, :] - self.landmarks[j, :]) < check_dist and \
-                        self.landmark_labels[i] == self.landmark_labels[j]:
-                    num_below += 1
-                    if j not in lms_to_delete:
-                        lms_to_delete += [j]
-        print("Number of landmarks less than {} meters apart with same label is: {}".format(check_dist, num_below))
-        print("Number of landmarks deleted: {}".format(len(lms_to_delete)))
+        # Mean shift clustering of landmarks for merging.
+        mean_shift_radius = 1.5
+        max_iters = 20
 
-        # Remove the landmarks that are too close to another one.
-        self.landmarks = np.delete(self.landmarks, lms_to_delete, axis=0)
-        self.landmark_labels = np.delete(self.landmark_labels, lms_to_delete)
+        new_landmarks = []
+        new_landmark_labels = []
 
-        print("Number of landmarks after merging: {}".format(self.landmarks.shape[0]))
+        while map_landmarks.shape[0] > 0:
+            mean = map_landmarks[0, :]
+
+            n_iters = 0
+            while True:
+                n_iters += 1
+                label = -1
+                lms_in_radius = []
+                new_mean = np.zeros((3,))
+                for i in range(0, map_landmarks.shape[0]):
+                    if np.linalg.norm(map_landmarks[i, :] - mean) < mean_shift_radius:
+                        if label == -1:
+                            label = map_landmark_labels[i]
+                        elif label != map_landmark_labels[i]:
+                            continue
+                        lms_in_radius.append(i)
+                        new_mean += map_landmarks[i, :]
+
+                new_mean = new_mean / len(lms_in_radius)
+
+                if np.equal(new_mean, mean).all() or n_iters >= max_iters:
+                    # print("Deleted {}".format(lms_in_radius))
+                    # print("Landmarks left: {}".format(map_landmarks.shape[0]))
+                    map_landmarks = np.delete(map_landmarks, lms_in_radius, axis=0)
+                    map_landmark_labels = np.delete(map_landmark_labels, lms_in_radius, axis=0)
+                    new_landmarks.append(new_mean)
+                    new_landmark_labels.append(label)
+                    break
+                else:
+                    mean = new_mean
+
+        print("Number of map landmarks deleted with merging: {}".format(n_landmarks_before - len(new_landmarks)))
+        self.landmarks = np.vstack(new_landmarks)
+        self.landmark_labels = np.array(new_landmark_labels)
 
         # Find the neighbors within range of each landmark:
         for i in range(self.landmarks.shape[0]):
@@ -293,6 +341,9 @@ class Map:
     def compute_triangles(self):
         print("Computing triangles.")
         tic = time.perf_counter()
+
+        # The minimum side length of a triangle, to prevent over matching (low side lengths are very common).
+        min_side_length = 1.
 
         # This is done to sort triangle indices according to their length. (Right hand rule)
         # Found no better way than to hack the shit out of this.
@@ -324,9 +375,10 @@ class Map:
                                 ])
                                 order = np.argsort(vector)
                                 vector = vector[order]
-                                order_map = order_mapping[tuple(order)]
-                                lms = [triangle[order_map[i]] for i in range(3)]
-                                self.triangles.update({triangle_key: [vector, lms]})
+                                if vector[-1] > min_side_length:
+                                    order_map = order_mapping[tuple(order)]
+                                    lms = [triangle[order_map[i]] for i in range(3)]
+                                    self.triangles.update({triangle_key: [vector, lms]})
 
         print("Triangle count is: {}".format(len(self.triangles)))
         toc = time.perf_counter()
@@ -345,18 +397,27 @@ class Map:
     def generate_tree(self):
         tic = time.perf_counter()
         for item in self.triangles.items():
-            self.octtree.add(item, item[1][0])
+            # item: {triangle_key, [vector, landmarks]}
+            labels_key = tuple([self.landmark_labels[item[1][1][i]] for i in range(3)])
+            if labels_key not in self.oct_trees:
+                self.oct_trees.update({labels_key: OctTree(min_bound=np.zeros((3,)),
+                                                           max_bound=np.ones((3,)) * self.max_dist,
+                                                           depth=7)})
+            self.oct_trees[labels_key].add(item, item[1][0])
 
         print("Time to generate oct tree: {}".format(time.perf_counter() - tic))
 
     def render(self, frame_landmarks, frame_labels):
-        renderer = LandmarkRenderer(self.landmarks, self.landmark_labels, frame_landmarks, frame_labels, get_colors())
+        renderer = LandmarkRenderer(self.landmarks, self.landmark_labels,
+                                    self.all_landmarks, self.all_landmark_labels,
+                                    frame_landmarks, frame_labels,
+                                    self.matched_lms, get_colors())
         renderer.run()
 
     def check_triangle(self):
         tic = time.perf_counter()
 
-        max_dist = 1.0
+        max_dist = 2.0
         num_landmarks = 8
 
         anchor = 0
@@ -411,12 +472,19 @@ class Map:
         print("Landmark count is: {}".format(len(test_nbh)))
         print("Test triangle count is: {}".format(len(test_triangles)))
 
+        triangles_matched = 0
+        max_tris_matched = 0
+
         t_matches = [[] for i in range(self.landmarks.shape[0])]
         lm_matches = [[] for i in range(self.landmarks.shape[0])]
         for i_triangle, item in enumerate(test_triangles.items()):
-            for element in self.octtree.query(item[1][0], l1_radius=max_dist):
+            labels_key = tuple([self.landmark_labels[item[1][1][i]] for i in range(3)])
+            triangles_matched_here = 0
+            for element in self.oct_trees[labels_key].query(item[1][0], l1_radius=max_dist):
                 matches = [[], [], []]
                 if np.linalg.norm(item[1][0] - element[1][0]) <= max_dist:
+                    triangles_matched_here += 1
+
                     for i in range(3):
                         lm_index_world = element[1][1][i]
                         if lm_index_world not in matches[i]:
@@ -429,6 +497,14 @@ class Map:
                         # if i_triangle not in t_matches[lm_index]:
                         #     t_matches[lm_index] += [i_triangle]
                         #     lm_matches[lm_index] += [item[1][1][i]]
+
+            triangles_matched += triangles_matched_here
+            max_tris_matched = max(max_tris_matched, triangles_matched_here)
+
+        print("Average amount of map triangles matched per frame triangle: {}".format(
+            triangles_matched / len(test_triangles)))
+        print("Maximum amount of map triangles matched per one frame triangle: {}".format(
+            max_tris_matched))
 
         match_counts = np.zeros((self.landmarks.shape[0],))
         match_indices = np.zeros((self.landmarks.shape[0],))
@@ -449,6 +525,10 @@ class Map:
         print("This query took {} seconds.".format(time.perf_counter() - tic))
 
     def query_frame(self, landmarks, labels):
+        tic = time.perf_counter()
+
+        labels[np.where(labels == 1)] = 15
+
         # This is done to sort triangle indices according to their length. (Right hand rule)
         # Found no better way than to hack the shit out of this.
         order_mapping = {
@@ -460,16 +540,18 @@ class Map:
             (2, 1, 0): (0, 2, 1)
         }
 
+        max_dist = 1.0
+
         maximum_length = 0
         frame_triangles = {}
         for i in range(landmarks.shape[0]):
             for nb_1 in range(i + 1, landmarks.shape[0]):
-                length_1 = np.linalg.norm(self.landmarks[i, :] - self.landmarks[nb_1, :])
+                length_1 = np.linalg.norm(landmarks[i, :] - landmarks[nb_1, :])
                 for nb_2 in range(landmarks.shape[0]):
                     if nb_2 == i or nb_2 == nb_1:
                         continue
 
-                    length_2 = np.linalg.norm(self.landmarks[nb_2, :] - self.landmarks[nb_1, :])
+                    length_2 = np.linalg.norm(landmarks[nb_2, :] - landmarks[nb_1, :])
 
                     triangle = [i, nb_1, nb_2]
                     triangle.sort()
@@ -478,7 +560,7 @@ class Map:
                         vector = np.array([
                             length_1,
                             length_2,
-                            np.linalg.norm(self.landmarks[i, :] - self.landmarks[nb_2, :])
+                            np.linalg.norm(landmarks[i, :] - landmarks[nb_2, :])
                         ])
                         order = np.argsort(vector)
                         vector = vector[order]
@@ -487,9 +569,72 @@ class Map:
                         frame_triangles.update({triangle_key: [vector, lms]})
                         maximum_length = max(maximum_length, np.max(vector))
 
-        print("Landmark count is: {}".format(landmarks.shape[0]))
+        print("Test landmark count is: {}".format(landmarks.shape[0]))
         print("Test triangle count is: {}".format(len(frame_triangles)))
         print("Maximum triangle side length is: {}".format(maximum_length))
+
+        triangles_matched = 0
+        max_tris_matched = 0
+
+        t_matches = [[] for i in range(self.landmarks.shape[0])]
+        lm_matches = [[] for i in range(self.landmarks.shape[0])]
+        for i_triangle, item in enumerate(frame_triangles.items()):
+            labels_key = tuple([labels[item[1][1][i]] for i in range(3)])
+            triangles_matched_here = 0
+            for element in self.oct_trees[labels_key].query(item[1][0], l1_radius=max_dist):
+                matches = [[], [], []]
+                if np.linalg.norm(item[1][0] - element[1][0]) <= max_dist:
+                    triangles_matched_here += 1
+
+                    for i in range(3):
+                        lm_index_world = element[1][1][i]
+                        if lm_index_world not in matches[i]:
+                            matches[i] += [lm_index_world]
+
+                for i in range(3):
+                    for lm in matches[i]:
+                        lm_index_frame = item[1][1][i]
+                        lm_matches[lm] += [lm_index_frame]
+                        # if i_triangle not in t_matches[lm_index]:
+                        #     t_matches[lm_index] += [i_triangle]
+                        #     lm_matches[lm_index] += [item[1][1][i]]
+
+            triangles_matched += triangles_matched_here
+            max_tris_matched = max(max_tris_matched, triangles_matched_here)
+
+        print("Average amount of map triangles matched per frame triangle: {}".format(
+            triangles_matched / len(frame_triangles)))
+        print("Maximum amount of map triangles matched per one frame triangle: {}".format(
+            max_tris_matched))
+
+        match_counts = np.zeros((self.landmarks.shape[0],))
+        match_indices = np.zeros((self.landmarks.shape[0],))
+        match_dict = {}
+        for i, match in enumerate(lm_matches):
+            most_common = Counter(match).most_common(1)
+            if most_common:
+                match_counts[i] = most_common[0][1]
+                match_indices[i] = most_common[0][0]
+
+            match_dict.update({i: [match_indices[i], match_counts[i]]})
+
+        argsorted = np.argsort(match_counts)
+        out = np.stack([match_indices[argsorted], argsorted, match_counts[argsorted]])
+        print("Match counts:")
+        print(np.transpose(np.flip(out, axis=1)[:, :30]))
+
+        self.matched_lms = argsorted[:30]
+
+        print("This query took {} seconds.".format(time.perf_counter() - tic))
+
+    def odometry_ransac(self, matches, frame_landmarks):
+        match_count = frame_landmarks.shape[0] * 3
+        min_support = 6
+
+        for i in range(match_count):
+            frame_index_1 = frame_index_2
+            for j in range(i + 1, match_count):
+                print("Not finished.") #if self.landmarks[j] - frame_landmarks
 
 
 def load_frame(path, frame_indices):
@@ -514,11 +659,11 @@ def load_frame(path, frame_indices):
 if __name__ == '__main__':
     lol = Map()
     f_lms, f_labels = load_frame("/home/felix/vision_ws/Semantic-Features/results/_results.txt", list(range(271)))
-    lol.load_landmarks(f_lms, f_labels)
-    lol.render(f_lms[::2], f_labels[::2])
+    lol.load_landmarks(f_lms[1:], f_labels[1:])
     lol.compute_triangles()
     # lol.plot_triangles()
     lol.generate_tree()
     lol.query_frame(f_lms[0], f_labels[0])
-    lol.check_triangle()
+    lol.render(f_lms, f_labels)
+    # lol.check_triangle()
 
