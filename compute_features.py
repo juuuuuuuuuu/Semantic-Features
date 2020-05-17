b5863c5fe07343f4bbae3dacd0d67b383bacd161
juliuserbach <erbachj@ethz.ch>
2020-05-17 22:48:54 +0200

    merge pole

--- compute_features.py
+++ compute_features.py
@@ -13,16 +13,16 @@ from tools import utils
 
 QUANTILE = True
 FIT_LINE = False
-FIT_BOX = False
-MERGE_BBOXES = True
+FIT_BOX = True
+MERGE_BBOXES = 1
 
 # Choose number of filter for mergedbbox
-mbboxnr = 1
+mbboxnr = 2
 
 # Choose error for merging bboxes
 inflation = 0.1
 
-num_filt = 2
+num_filt = 3
 
 
 def isOverlapping1D(xmin1, xmin2, xmax1, xmax2) :
@@ -91,7 +91,7 @@ def fit_line(pcl, class_id):
             if inlier_mask.sum() > d:
                 x_model = x[inlier_mask].mean()
                 z_model = z[inlier_mask].mean()
-
+                    
                 x_error = abs(x_model - x[inlier_mask])
                 z_error = abs(z_model - z[inlier_mask])
                 error = (x_error**2 + z_error**2).mean()**0.5
@@ -117,10 +117,10 @@ def fit_box(pcl, class_id):
     indices = list(range(x.shape[0]))
     # Define minimum number of iterations N
     # N = int(math.log(1-p)/math.log(1-(1-e)**2))
-    N = int(0.3 * len(x))
+    N = int(0.5 * len(x))
     max_iter = 20
     # Define error thresholds
-    e = 0.1
+    e = 0.15
     density_opt = 0
     inlier_opt = np.array([False]*len(x))
     # Subsample minimum number of datapoints to create model 
@@ -162,7 +162,57 @@ def fit_box(pcl, class_id):
     y = np.where(inlier_opt, y, np.nan)
     z = np.where(inlier_opt, z, np.nan)
     return x, y, z
-    
+
+def merge_pole(pcls, bboxs, classes_list):
+    error_dist = 2
+    bboxs = bboxs[:,:,mbboxnr]
+    x = []
+    y = []
+    z_min = []
+    z_max = []
+    for i, pcl in enumerate(pcls):
+        pcl = pcl[:,:,mbboxnr]
+        pcl = pcl[~np.isnan(pcl)].reshape(4, -1)
+        if pcl.size > 0:
+            x_t, y_t, _, _ = pcl.mean(axis=1)
+            _, _, z_min_t, _ = pcl.min(axis=1)
+            _, _, z_max_t, _ = pcl.max(axis=1)
+            x.append(x_t)
+            y.append(y_t)
+            z_min.append(z_min_t)
+            z_max.append(z_max_t)
+    x = np.asarray(x)
+    y = np.asarray(y)
+    z_min = np.asarray(z_min).reshape(-1, 1)
+    z_max = np.asarray(z_max).reshape(-1, 1)
+    xx = np.meshgrid(x, x)
+    yy = np.meshgrid(y, y)
+    dist = ((xx[0]-xx[1])**2 + (yy[0]-yy[1])**2)**0.5
+    close = np.where(dist<error_dist, True, False)
+    indices = np.argsort(close.sum(axis=0))[::-1]
+    x_mean = np.zeros(x.shape)
+    y_mean = np.zeros(x.shape)
+    z_min_m = np.zeros(x.shape)
+    z_max_m = np.zeros(x.shape)
+    bboxs_merged = []
+    class_list_out = []
+    merged = np.zeros(x.shape, dtype=bool)
+    for i in indices:
+        neighbors = close[i]
+        if not merged[i] and sum(neighbors) > 1 and classes_list[i]==10:
+            x_mean = x[neighbors].mean()
+            y_mean = y[neighbors].mean()
+            z_min_m = z_min[neighbors].min()
+            z_max_m = z_max[neighbors].max()
+            bboxs_merged.append(np.array([x_mean - 0.1, y_mean - 0.1, z_min_m, x_mean + 0.1, y_mean + 0.1,  z_max_m]).reshape(1,6))
+            class_list_out.append(classes_list[i])
+            merged = np.logical_or(merged, neighbors)
+        elif not merged[i]:
+            bboxs_merged.append(bboxs[i].reshape(1,6))
+            merged = np.logical_or(merged, neighbors)
+            class_list_out.append(classes_list[i])
+    return bboxs_merged, class_list_out    
+
 
 def expand_concat(u, v, z, u_m, v_m, z_m):
     if u.ndim == 1:    
@@ -193,7 +243,7 @@ def get_bbox(pcl):
 
 if __name__ == '__main__':
     basedir = 'content/kitti_dataset/dataset'
-    sequence = '04'
+    sequence = '08'
 
     dataset = pykitti.odometry(basedir, sequence)
     baseline = dataset.calib.b_rgb
@@ -202,6 +252,7 @@ if __name__ == '__main__':
     f_y = P_cam2[1, 1]
     u_0 = P_cam2[0, 2]
     v_0 = P_cam2[1, 2]
+    print(P_cam2)
 
     # World is twisted so we need to transform.
     T_w0_w = np.array([[ 0., 0., 1., 0.],
@@ -223,11 +274,12 @@ if __name__ == '__main__':
     for x in sorted(all_data, key = itemgetter('image_id')):
         all_data_sort.append(x)
 
-    path = "content/kitti_dataset/dataset/sequences/04"
+    path = "content/kitti_dataset/dataset/sequences/08"
     out_path = "results"
 
     results = []
     # store bboxes and pcls
+    
     if MERGE_BBOXES:
         bboxes = []
         pcls = []
@@ -236,9 +288,9 @@ if __name__ == '__main__':
 
     for n, data in enumerate(all_data_sort):
         # Only processing half of the images
-        if n % 2 == 0:
-            print("Skip processing frame " + data['image_id'] + '.')
-            continue
+        # if n % 2 == 0:
+        #     print("Skip processing frame " + data['image_id'] + '.')
+        #     continue
 
         frame_id = int(data['image_id'])
         print("Processing frame " + data['image_id'] + '.')
@@ -320,40 +372,69 @@ if __name__ == '__main__':
             np.save(bbox_path, bbox, allow_pickle=False)
             results.append([frame_id, i, class_ids[i], pcl_path, bbox_path, transform[:3, 3]])
 
-    if MERGE_BBOXES:
 
+
+
+    
+    
+    if MERGE_BBOXES==1:
         npbboxes = np.asarray(bboxes)
+        mergedbboxes, class_list_out = merge_pole(pcls, npbboxes, classes_list)
+        print(len(mergedbboxes))
+        # Save pcls and mergedbboxes
+        mergedbbox_path = os.path.join(out_path, "mergedbbox")
+        np.save(mergedbbox_path, np.asarray(mergedbboxes), allow_pickle=False)
+        classes_list_path = os.path.join(out_path, "classes_list")
+        np.save(classes_list_path, np.array(class_list_out), allow_pickle=False)
+        results.sort()
+        with open(os.path.join(out_path, "_results.txt"), 'w') as f:
+            for result in results:
+                f.write(str(result[0]) + " " + str(result[1]) + " " + str(result[2]) + " " + str(result[3]) + " " +
+                        str(result[4]) + " " + str(result[5][0]) + " " + str(result[5][1]) + " " + str(result[5][2]) + "\n")
+        print("Finished.")
+    elif MERGE_BBOXES==2:
+        if False:
+            npbboxes = np.asarray(bboxes)
 
-        # Number of unmerged landmarks
-        nlandm = len(pcls)
+            # Number of unmerged landmarks
+            nlandm = len(pcls)
 
-        # Adjaceny matrix of bbox, if bbox intersect that entry gets 1, otherwise 0
-        index = np.zeros((nlandm, nlandm))
+            # Adjaceny matrix of bbox, if bbox intersect that entry gets 1, otherwise 0
+            index = np.zeros((nlandm, nlandm))
 
-        # All merged bboxes
-        mergedbboxes = []
-        for i in range(nlandm):
-            for j in range(nlandm):
-                if isOverlapping3D(npbboxes[i, :, mbboxnr], npbboxes[j, :, mbboxnr]) and classes_list[i] == classes_list[j]:
-                    index[i, j] = 1
+            # All merged bboxes
+            mergedbboxes = []
+            for i in range(nlandm):
+                for j in range(nlandm):
+                    if isOverlapping3D(npbboxes[i, :, mbboxnr], npbboxes[j, :, mbboxnr]) and classes_list[i] == classes_list[j]:
+                        index[i, j] = 1
 
-        # Find for all intersecting bboxes minimum and maximum
-        blacklist = np.array((-1,))
-        class_list_out = []
-        for i in range(nlandm):
-            if i in blacklist:
-                continue
-            elif np.sum(index[i,:]) != 0:
-                minoverlappingbboxes = npbboxes[np.where(index[i,:]==1),0:3,mbboxnr]
-                maxoverlappingbboxes = npbboxes[np.where(index[i,:]==1),3:6, mbboxnr]
+            # Find for all intersecting bboxes minimum and maximum
+            blacklist = np.array((-1,))
+            class_list_out = []
+            for i in range(nlandm):
+                if i in blacklist:
+                    continue
+                elif np.sum(index[i,:]) != 0:
+                    minoverlappingbboxes = npbboxes[np.where(index[i,:]==1),0:3,mbboxnr]
+                    maxoverlappingbboxes = npbboxes[np.where(index[i,:]==1),3:6, mbboxnr]
 
-                con = np.concatenate([np.min(minoverlappingbboxes, axis=1), np.max(maxoverlappingbboxes, axis=1)],
-                                         axis=1)
-                mergedbboxes.append(con)
-                blacklist_add = np.where(index[i,:]==1)[0]
-                blacklist = np.unique(np.concatenate((blacklist,blacklist_add),0))
+                    con = np.concatenate([np.min(minoverlappingbboxes, axis=1), np.max(maxoverlappingbboxes, axis=1)],
+                                            axis=1)
+                    mergedbboxes.append(con)
+                    blacklist_add = np.where(index[i,:]==1)[0]
+                    blacklist = np.unique(np.concatenate((blacklist,blacklist_add),0))
 
-                class_list_out.append(classes_list[i])
+                    class_list_out.append(classes_list[i])
+        else:
+            npbboxes = np.asarray(bboxes)
+            nppcls = np.asarray(pcls)
+            for i, pcl in enumerate(pcls):
+                if classes_list[i] == 11: # if it is a pole
+                    merge_pole(pcls, npbboxes)
+
+
+
         print(len(mergedbboxes))
         # Save pcls and mergedbboxes
         mergedbbox_path = os.path.join(out_path, "mergedbbox")
@@ -367,6 +448,3 @@ if __name__ == '__main__':
                     str(result[4]) + " " + str(result[5][0]) + " " + str(result[5][1]) + " " + str(result[5][2]) + "\n")
 
     print("Finished.")
-
-
-
